db1e480fc1ac2bde01e97ad7682c0e4f557e8a9b
Jia <angersax@sina.com>
2022-05-21 11:43:57 +0800

    fix(test): fix test fail on incorrect path

--- script/test.sh
+++ script/test.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-list="$(find tests/* -maxdepth 0 -type d | grep -v __pycache__)"
-coverage run --source=api,tests -m pytest -v --capture=no "$list"
+coverage run --source=api,tests -m pytest
